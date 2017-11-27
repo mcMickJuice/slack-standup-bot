b4862f512d15ddfa3b2e74bbb1289e5e87fc4d49
Mike Joyce <mikejoyce19@gmail.com>
2017-11-26 19:33:47 -0600

    project setup

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('hello!');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, (err: Error) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`App started at ${port}`);
+});
